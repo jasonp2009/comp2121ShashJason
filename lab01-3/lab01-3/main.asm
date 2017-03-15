d861633aec94172f0c724e38c84bb9051eebadd6
jasonp2009 <jason_pawar@hotmail.com>
2017-03-15 13:59:02 +1100

    Fixed the upper bound potential error
    Added testing if the letter is above lowercase z, before those
    characters would be treated as if they were lowercase letters.

--- lab01-3/lab01-3/main.asm
+++ lab01-3/lab01-3/main.asm
@@ -1,20 +1,26 @@
 .include "m2560def.inc"
 
+.set UPPERCASE_A = 65
+.set LOWERCASE_A = 97
+.set STRING_LEN = 20
+.set NUM_LETTERS = 26
+
 .dseg
 	string: .byte 20 ;reserve 20 bytes in data memory
 
 .cseg
-	string_value: .db "hello world",0,0,0,0,0,0,0,0,0 ;store string in program memory
+	string_value: .db "{ello worl}",0,0,0,0,0,0,0,0,0 ;store string in program memory
 
 	ldi ZH, high(string_value) ;initialise Z to pont to program memory
 	ldi ZL, low(string_value)
 	ldi XH, high(string) ;initialise X to pont to reserved data memory
 	ldi XL, low(string)
 
-	ldi r16, 20	 ;max 20 letters
+	ldi r16, STRING_LEN
 	; Made a change below because say input = a, a - 97 = 0, +65 = A
-	ldi r17, 65  ;Uppercase A
-	ldi r18, 97	 ;lowercase a 
+	ldi r17, UPPERCASE_A
+	ldi r18, LOWERCASE_A
+	ldi r20, NUM_LETTERS
 	lpm r19, Z+	 ;load first character of Z string
 
 	LOOP:
@@ -33,11 +39,20 @@
 				lpm r19, Z+ ;load next Z value
 				rjmp LOOP
 			ELSE:
-				add r19, r17 ;r19+(what we subtracted+32[for uppercase]=129)
-				st X+, r19 ;store new r19 value into RAM(data memory)
-				lpm r19, Z+ ;load next Z value
-				rjmp LOOP
-				
+				sub r19, r20
+				brsh OTHER
+					add r19, r20
+					add r19, r17 ;r19+(what we subtracted+32[for uppercase]=129)
+					st X+, r19 ;store new r19 value into RAM(data memory)
+					lpm r19, Z+ ;load next Z value
+					rjmp LOOP
+
+				OTHER:
+					add r19, r20
+					add r19, r18
+					st X+, r19
+					lpm r19, Z+
+					rjmp LOOP
 				
 	END:
 		rjmp halt
