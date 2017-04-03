7a6493c6b0aaa2e1a77071e6987ab7bfe5b2170c
jasonp2009 <jason_pawar@hotmail.com>
2017-04-03 14:49:01 +1000

    Possibly completed macro

--- lab02-3/lab02-3/main.asm
+++ lab02-3/lab02-3/main.asm
@@ -9,16 +9,11 @@
 ; Replace with your application code
 
 .set NEXT_STRING = 0x0000
-.macro defstring ; str
+.macro defint ; str
 	.set T = PC ; save current position in program memory
 	.dw NEXT_STRING << 1 ; write out address of next list node
 	.set NEXT_STRING = T ; update NEXT_STRING to point to this node
-	
-	.if strlen(@0) & 1 ; odd length + null byte
-		.db @0, 0
-	.else ; even length + null byte, add padding byte
-		.db @0, 0, 0
-	.endif
+	.dw @0
 .endmacro
 
 start:
