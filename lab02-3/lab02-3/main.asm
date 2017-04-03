7593cb3b432cefbf6c4cdf2ef7dd266bc27853ef
jasonp2009 <jason_pawar@hotmail.com>
2017-04-03 13:22:33 +1000

    Added given macro for strings
    The macro needs to be changed to accept 16-bit integers

--- lab02-3/lab02-3/main.asm
+++ lab02-3/lab02-3/main.asm
@@ -7,6 +7,20 @@
 
 
 ; Replace with your application code
+
+.set NEXT_STRING = 0x0000
+.macro defstring ; str
+	.set T = PC ; save current position in program memory
+	.dw NEXT_STRING << 1 ; write out address of next list node
+	.set NEXT_STRING = T ; update NEXT_STRING to point to this node
+	
+	.if strlen(@0) & 1 ; odd length + null byte
+		.db @0, 0
+	.else ; even length + null byte, add padding byte
+		.db @0, 0, 0
+	.endif
+.endmacro
+
 start:
     inc r16
     rjmp start
