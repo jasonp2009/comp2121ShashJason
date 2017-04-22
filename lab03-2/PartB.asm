a35c472a725715b743880e32bcd0d7316b5e2db5
Shash98 <shashikar98@gmail.com>
2017-04-22 19:16:34 +1000

    Part B DONE
    had to double check part a, made no changes to it

--- lab03-2/PartB.asm
+++ lab03-2/PartB.asm
@@ -3,7 +3,7 @@
 
 .equ PATTERN = 0b1011001110001111
 .def temp = r16 
-//.def leds = r17
+
 
 ; The macro clears a word (2 bytes) in a memory 
 ; the parameter @0 is the memory address for that word
@@ -22,6 +22,7 @@ TempCounter:
 	.byte 2 ; Temporary counter. Used to determine if one second has passed
 
 .cseg
+	ldi r21, 0
 .org 0x0000
 	jmp RESET
 	jmp DEFAULT ; No handling for IRQ0.
@@ -38,7 +39,7 @@ RESET:
 	ldi temp, low(RAMEND) 
 	out SPL, temp 
 	ser temp ; set Port C as output 
-	out DDRC, temp 
+	out DDRC, temp
 	rjmp main
 
 Timer0OVF: ; interrupt subroutine to Timer0 
@@ -51,21 +52,160 @@ Timer0OVF: ; interrupt subroutine to Timer0
 	lds r24, TempCounter 
 	lds r25, TempCounter+1 
 	adiw r25:r24, 1 ; Increase the temporary counter by one.
-
 	cpi r24, low(7812) ; Check if (r25:r24) = 7812 
 	ldi temp, high(7812) ; 7812 = 106/128 
 	cpc r25, temp 
-	brne NotSecond 
-	//com leds 
-	//out PORTC, leds 
-	com XL
-	com XH
-	out PORTC, XL
+	brne Compbreak
+	rjmp Comp
+Compbreak:
+	rjmp NotSecond 
+
+Comp:
+	cpi r21, 0
+	breq Zero
+	cpi r21, 1
+	breq One
+	cpi r21, 2
+	breq Two
+	cpi r21, 3
+	breq Three
+	cpi r21, 4
+	breq Four	
+	cpi r21, 5
+	breq Five
+	cpi r21, 6
+	breq Six
+	cpi r21, 7
+	breq Seven
+	cpi r21, 8
+	breq Eight
+	rjmp Cont2
+
+Zero:
+	ldi r21, 0
+	ldi r22, low(PATTERN)
+	out PORTC, r22
+	inc r21	
+	rjmp Cont
+One:
+	ldi r22, low(PATTERN>>1)
+	out PORTC, r22
+	inc r21	
+	rjmp Cont
+Two:
+	ldi r22, low(PATTERN>>2)
+	out PORTC, r22
+	inc r21	
+	rjmp Cont
+Three:
+	ldi r22, low(PATTERN>>3)
+	out PORTC, r22
+	inc r21	
+	rjmp Cont
+Four:
+	ldi r22, low(PATTERN>>4)
+	out PORTC, r22
+	inc r21	
+	rjmp Cont
+Five:
+	ldi r22, low(PATTERN>>5)
+	out PORTC, r22
+	inc r21	
+	rjmp Cont
+Six:
+	ldi r22, low(PATTERN>>6)
+	out PORTC, r22
+	inc r21	
+	rjmp Cont
+Seven:
+	ldi r22, low(PATTERN>>7)
+	out PORTC, r22
+	inc r21	
+	rjmp Cont
+Eight:
+	ldi r22, low(PATTERN>>8)
+	out PORTC, r22
+	inc r21	
+	rjmp Cont
+	
+Cont2:
+	cpi r21, 9
+	breq Nine
+	cpi r21, 10
+	breq Ten
+	cpi r21, 11
+	breq Eleven	
+	cpi r21, 12
+	breq Tweleve
+	cpi r21, 13
+	breq Thirteen	
+	cpi r21, 14
+	breq Fourteen	
+	cpi r21, 15
+	breq Fifteen
+	cpi r21, 16
+	breq Sixteen
+Nine:
+	ldi r22, low(PATTERN>>9)
+	ldi r23, low(PATTERN<<7)
+	add r22, r23
+	out PORTC, r22
+	inc r21	
+	rjmp Cont
+Ten:
+	ldi r22, low(PATTERN>>10)
+	ldi r23, low(PATTERN<<6)
+	add r22, r23
+	out PORTC, r22
+	inc r21	
+	rjmp Cont
+Eleven:
+	ldi r22, low(PATTERN>>11)
+	ldi r23, low(PATTERN<<5)
+	add r22, r23
+	out PORTC, r22
+	inc r21	
+	rjmp Cont
+Tweleve:
+	ldi r22, low(PATTERN>>12)
+	ldi r23, low(PATTERN<<4)
+	add r22, r23
+	out PORTC, r22
+	inc r21	
+	rjmp Cont
+Thirteen:
+	ldi r22, low(PATTERN>>13)
+	ldi r23, low(PATTERN<<3)
+	add r22, r23
+	out PORTC, r22
+	inc r21	
+	rjmp Cont
+Fourteen:
+	ldi r22, low(PATTERN>>14)
+	ldi r23, low(PATTERN<<2)
+	add r22, r23
+	out PORTC, r22
+	inc r21	
+	rjmp Cont
+Fifteen:
+	ldi r22, low(PATTERN>>15)
+	ldi r23, low(PATTERN<<1)
+	add r22, r23
+	out PORTC, r22
+	inc r21	
+	rjmp Cont
+Sixteen:
+	ldi r21, 0
+	ldi r22, low(PATTERN)
+	out PORTC, r22
+	inc r21	
+	rjmp Cont
+
+Cont:
 	clear TempCounter ; Reset the temporary counter. Load the value of the second counter. 
 	lds r24, SecondCounter 
 	lds r25, SecondCounter+1 
 	adiw r25:r24, 1 ; Increase the second counter by one.
-
 	sts SecondCounter, r24 
 	sts SecondCounter+1, r25 
 	rjmp EndIF
@@ -84,12 +224,6 @@ EndIF:
 	reti ; Return from the interrupt.
 
 main: 
-	//ldi leds, 0xFF 
-	//out PORTC, leds 
-	//ldi leds, PATTERN 
-	ldi XL, low(PATTERN)
-	ldi XH, high(PATTERN)
-
 	clear TempCounter ; Initialize the temporary counter to 0 
 	clear SecondCounter ; Initialize the second counter to 0 
 	ldi temp, 0b00000000 
