7ffe5c96410a3c0758a143bfe38fc0bf0d92215d
Shash98 <shashikar98@gmail.com>
2017-05-21 20:22:08 +1000

    part a almost done
    need to print rpm i think

--- lab05-1/PartA.asm
+++ lab05-1/PartA.asm
@@ -1,79 +1,70 @@
-.include "m2560def.inc" 
+.include "m2560def.inc"
 
-.def rpm = r22
-.def temp = r17
-.def temp1 = r20 
-.def temp2 = r21
-//.def acc   = r22
-.def input = r23
-//.def press = r18
-.def count = r19
+.def temp = r16
+.def temp1 = r17 
+.def temp2 = r18
+.def rpm = r19
+.def lcd = r20	; lcd handle
+.def count = r21
 
-.equ PORTADIR = 0xF0 ; PD7-4: output, PD3-0, input 
+.macro clear
+    ldi YL, low(@0)    
+    ldi YH, high(@0)
+    clr temp 
+    st Y+, temp     
+    st Y, temp
+.endmacro
+                        
+.dseg
+TempCounter:
+   .byte 2
 
+; LCD macros
 .macro do_lcd_command
-	ldi r16, @0
+	ldi lcd, @0
 	rcall lcd_command
 	rcall lcd_wait
 .endmacro
 .macro do_lcd_data
-	ldi r16, @0
+	ldi lcd, @0
 	rcall lcd_data
 	rcall lcd_wait
 .endmacro
 .macro do_lcd_num
-	mov r16, @0
-	subi r16, -'0'
+	mov lcd, @0
+	subi lcd, -'0'
 	rcall lcd_data
 	rcall lcd_wait
 .endmacro
-; The macro clears a word (2 bytes) in a memory 
-; the parameter @0 is the memory address for that word
-.macro clear 
-	ldi YL, low(@0) ; load the memory address to Y 
-	ldi YH, high(@0) 
-	clr temp 
-	st Y+, temp ; clear the two bytes at @0 in SRAM 
-	st Y, temp 
-.endmacro
-
-.dseg
-SecondCounter:
-	.byte 2 ; Two-byte counter for counting seconds.
-TempCounter:
-	.byte 2 ; Temporary counter. Used to determine if one second has passed
 
 .cseg
 .org 0x0000
-    jmp RESET
+   jmp RESET
+   jmp DEFAULT         
+   jmp DEFAULT        
 .org INT2addr
     jmp EXT_INT2
 .org OVF0addr
-    jmp Timer0OVF        ; Jump to the interrupt handler for Timer0 overflow.
-//	jmp DEFAULT          ; default service for all other interrupts.
-//DEFAULT:  reti          ; no service
-//.org 0x200
-
+   jmp Timer0OVF      
+jmp DEFAULT 
+DEFAULT:  reti          
 
 RESET: 
-	ldi r21, 0
- 	ldi rpm, 0
-
-	ldi temp, high(RAMEND) ; Initialize stack pointer
+    ldi temp, high(RAMEND) 	; Initialize stack pointer
     out SPH, temp
     ldi temp, low(RAMEND)
     out SPL, temp
     ser temp
-    out DDRC, temp ; set Port C as output
+    out DDRC, temp 			; set Port C as output
+	sei
 
-	ldi input, 0
-
-	ser r16
-	out DDRF, r16
-	out DDRA, r16
-	clr r16
-	out PORTF, r16
-	out PORTA, r16
+	; LCD setup
+	ser temp
+	out DDRF, temp
+	out DDRA, temp
+	clr temp
+	out PORTF, temp
+	out PORTA, temp
 
 	do_lcd_command 0b00111000 ; 2x5x7
 	rcall sleep_5ms
@@ -85,81 +76,88 @@ RESET:
 	do_lcd_command 0b00000001 ; clear display
 	do_lcd_command 0b00000110 ; increment, no display shift
 	do_lcd_command 0b00001110 ; Cursor on, bar, no blink
-	
-	do_lcd_data '0'
+
+	do_lcd_data 'r';
+	do_lcd_data 'p';
+	do_lcd_data 'm';
+	do_lcd_data ':';
+	do_lcd_data ' ';
 
 	rjmp main
 
 EXT_INT2:
 	inc rpm
-	do_lcd_data 'C'
-	do_lcd_data 'C'
-	do_lcd_data 'C'
-	do_lcd_data 'C'
-	do_lcd_data 'C'
-	do_lcd_data 'C'
-	do_lcd_data 'C'
-	rjmp loop2
+	reti
 
-Timer0OVF: ; interrupt subroutine to Timer0 
-	in temp, SREG 
-	push temp ; Prologue starts. 
-	push YH ; Save all conflict registers in the prologue. 
-	push YL 
-	push r25 
-	push r24 ; Prologue ends. Load the value of the temporary counter. 
-	lds r24, TempCounter 
-	lds r25, TempCounter+1 
-	adiw r25:r24, 1 ; Increase the temporary counter by one.
-	cpi r24, low(1953) ; Check if (r25:r24) = 7812 
-	ldi temp, high(1953) ; 7812 = 106/128 
-	cpc r25, temp 
-	brne Compbreak
-	rjmp Continue
-Compbreak:
-	rjmp NotSecond 
+Timer0OVF:
+    in temp, SREG
+    push temp      
+    push YH         
+    push YL
+    push r25
+    push r24
 
-Continue:
-	clear TempCounter ; Reset the temporary counter. Load the value of the second counter. 
-	lds r24, SecondCounter 
-	lds r25, SecondCounter+1 
-	adiw r25:r24, 1 ; Increase the second counter by one.
-	sts SecondCounter, r24 
-	sts SecondCounter+1, r25 
-	inc rpm
-	rjmp reset_LCD
-	rjmp EndIF
-	 
-NotSecond: ; Store the new value of the temporary counter. 
-	sts TempCounter, r24 
-	sts TempCounter+1, r25 
-	
-EndIF: 
-	//ldi rpm, 0
-	pop r24 ; Epilogue starts; 
-	pop r25 ; Restore all conflict registers from the stack. 
-	pop YL 
-	pop YH 
-	pop temp 
-	out SREG, temp 
-	reti ; Return from the interrupt.
+    lds r24, TempCounter
+	lds r25, TempCounter+1
+	adiw r25:r24, 1 
+	cpi r24, low(1953)      ; 1953 is 1/4 of 7812
+	ldi temp, high(1953)    
+	cpc r25, temp
+	brne NotSecond
+		
+	secondPassed: ; 1/4 of a second passed
+		do_lcd_command 0b00000001 ; clear display
+		do_lcd_command 0b00000110 ; increment, no display shift
+		do_lcd_command 0b00001110 ; Cursor on, bar, no blink
+
+		do_lcd_data 'r';
+		do_lcd_data 'p';
+		do_lcd_data 'm';
+		do_lcd_data ':';
+		do_lcd_data ' ';
+
+		out PORTC, rpm
+
+		//PRINT RPM
+
+		clr rpm
+		clear TempCounter
+    rjmp EndIF
+
+NotSecond:
+    sts TempCounter, r24
+    sts TempCounter+1, r25 
+
+EndIF:
+	pop r24        
+    pop r25         
+    pop YL
+    pop YH
+    pop temp
+    out SREG, temp
+    reti            ; Return from the interrupt.
 
 main:
-	clear TempCounter ; Initialize the temporary counter to 0 
-	clear SecondCounter ; Initialize the second counter to 0 
-	ldi temp, 0b00000000 
-	out TCCR0A, temp 
-	ldi temp, 0b00000010 
-	out TCCR0B, temp ; Prescaling value=8 
-	ldi temp, 1<<TOIE0 ; = 128 microseconds 
-	sts TIMSK0, temp ; T/C0 interrupt enable 
-	sei ; Enable global interrupt 
+    clear TempCounter       ; Initialize the temporary counter to 0
+
+	ldi temp, (2 << ISC20)      ; set INT0 as falling-
+    sts EICRA, temp             ; edge triggered interrupt
+    in temp, EIMSK              ; enable INT2
+    ori temp, (1<<INT2)
+    out EIMSK, temp
+
+	; Timer0 initilaisation
+    ldi temp, 0b00000000
+    out TCCR0A, temp
+    ldi temp, 0b00000010
+    out TCCR0B, temp        ; Prescaling value=8
+    ldi temp, 1<<TOIE0      ; = 128 microseconds
+    sts TIMSK0, temp        ; T/C0 interrupt enable
+
+    sei                     ; Enable global interrupt
+                            
+loop2: ; loop forever
 	rjmp loop2
-
-loop2:
-	rjmp loop2
-
-
 
 
 reset_LCD:
@@ -272,4 +270,3 @@ sleep_5ms:
 	rcall sleep_1ms
 	rcall sleep_1ms
 	ret
-
