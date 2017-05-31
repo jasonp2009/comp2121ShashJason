fabb3ad90e2ea15d76facfbbd94776e8dae9b2a2
Shash98 <shashikar98@gmail.com>
2017-05-31 15:00:16 +1000

    .

--- Ass3/VendingMachine/VendingMachine/Keyboard.asm
+++ Ass3/VendingMachine/VendingMachine/Keyboard.asm
@@ -1,65 +1,104 @@
 KEYBOARD:
-	ldi cmask, INITCOLMASK	; initial column mask (1110 1111)
-	clr col 				; initial column (0)
+	ldi cmask, INITCOLMASK  ; initial column mask
+    clr col                 ; initial column
 
 colloop:
-	cpi col, 4 				; compare current column # to total # columns
-	breq return				; if all keys are scanned, repeat
+    cpi col, 4
+    breq return               ; If all keys are scanned, repeat.
+    sts PORTL, cmask        ; Otherwise, scan a column.
+  
+    ldi temp1, 0xFF         ; Slow down the scan operation.
 
-	sts PORTL, cmask		; otherwise, scan a column
-	ldi temp1, 0xFF			; slow down the scan operation to debounce button press
 delay:
-	dec temp1
-	brne delay
+    dec temp1
+    brne delay              ; until temp1 is zero? - delay
 
-	lds temp1, PINL			; read PORTL
-	andi temp1, ROWMASK		; get the keypad output value
-	cpi temp1, 0xF0 		; check if any row is low (0)
-	breq rowloop			; if yes, find which row is low
-	ldi rmask, INITROWMASK	; initialize rmask with 0000 0001 for row check
-	clr row
+    lds temp1, PINL          ; Read PORTL
+    andi temp1, ROWMASK     ; Get the keypad output value
+    cpi temp1, 0xF          ; Check if any row is low
+    breq nextcol            ; if not - switch to next column
 
+                            ; If yes, find which row is low
+    ldi rmask, INITROWMASK  ; initialize for row check
+    clr row
+
+; and going into the row loop
 rowloop:
-	cpi row, 4 				; compare current value of row with total number of rows (4)
-	breq nextcol			; if theyre equal, the row scan is over.
-	mov temp2, temp1 		; temp1 is 0xF
-	and temp2, rmask 		; check un-masked bit
-	breq convert 			; if bit is clear, the key is pressed
-	inc row 				; else move to the next row
-	lsl rmask 				; shift row mask left by one
-	jmp rowloop
-
-nextcol:					; if row scan is over
-	lsl cmask 				; shift column mask left by one
-	inc col 				; increase column value
-	jmp colloop				; go to the next column
-
+    cpi row, 4              ; is row already 4?
+    breq nextcol            ; the row scan is over - next column
+    mov temp2, temp1
+    and temp2, rmask        ; check un-masked bit
+    breq convert            ; if bit is clear, the key is pressed
+    inc row                 ; else move to the next row
+    lsl rmask
+    jmp rowloop
+    
+nextcol:                    ; if row scan is over
+     lsl cmask
+     inc col                ; increase col value
+     jmp colloop            ; go to the next column
+     
 convert:
-	cpi col, 3				; if the pressed key is in col.3 
-	breq return			; we have a letter, so ignore it and restart
-	cpi row, 3				; if the key is not in col 3 and is in row3,
-	breq symbols			; we have a symbol or 0
-	mov temp1, row 			; otherwise we have a number in 1-9
-	lsl temp1 				; multiply temp1 by 2
-	add temp1, row 			; add row again to temp1 -> temp1 = row * 3
-	add temp1, col 			; temp1 = col*3 + row
-	subi temp1, -1			; add 1
-	; 1 row 0 col 0 -> temp1 = 0 + 0 + 1 = 1 = 0b 0000 0001
-	; 2 row 0 col 1 -> temp1 = 0 + 1 + 1 = 2 = 0b 0000 0010
-	; 3 row 0 col 2 -> temp1 = 0 + 2 + 1 = 3 = 0b 0000 0011
-	; 4 row 1 col 0 -> temp1 = 3 + 0 + 1 = 4 = 0b 0000 0100
-	jmp convert_end
+	; NOTE: cols and rows are counter-intuitive (flipped)
+	mov temp, col
+	mov col, row
+	mov row, temp
+
+	;out PORTC, col
+
+    cpi col, 3              ; If the pressed key is in col 3
+    breq letters           ; we have letter
+    ;breq convert_end
+                            ; If the key is not in col 3 and
+    cpi row, 3              ; if the key is in row 3,
+    breq symbols            ; we have a symbol or 0
+	;breq convert_end
+
+    ;mov temp1, row          ; otherwise we have a number 1-9
+    ;lsl temp1
+	;add col, 1
+	;add row, 1
+    ;add temp1, col
+    ;add temp1, row          ; temp1 = 1 + row + col
+    ;subi temp1, -'1'        ; add the value of character '1'
+    mov temp1, row          ; otherwise we have a number 1-9
+    lsl temp1
+    add temp1, row
+    add temp1, col          ; temp1 = row*3 + col
+	subi temp1, -1
+    jmp convert_end
+    
+letters:
+    ;ldi temp1, 'A'
+    ;add temp1, row          ; Get the ASCII value for the key
+	;clr temp1
+	cpi row, 3
+	breq nextcol
+	ldi temp1, 20
+    jmp convert_end
 
 symbols:
-	cpi col, 1 				; if its in column 1, it's a zero
-	brne return 			; ignore * and #
-	ldi temp1, 0
+    cpi col, 0              ; Check if we have a star
+    breq star
+    cpi col, 1              ; or if we have zero
+    breq ze_ro
+    ;ldi temp1, '#'         ; if not we have hash
+	;clr temp1				; TEMP: not handling the hash now
+	ldi temp1, 20
+    jmp convert_end
+star:
+    ;ldi temp1, '*'          ; set to star
+	;clr temp1
+	ldi temp1, 20
+    jmp convert_end
+ze_ro:
+    ldi temp1, 255          ; set to zero in binary
 	jmp convert_end
 
 convert_end:
 	mov flag, temp1
-	ret
+    ret
 
 return:
-	ldi flag, 255
+	ldi flag, 0
 	ret
